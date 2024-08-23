b421be7516939ad8b2b24ccdf59cbf9c793c476d
Tasneem-Burhan <batterywala.tasneem@gmail.com>
2024-08-23 13:24:52 +0530

    first commit1

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -3,6 +3,7 @@ import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
 import InvoiceStatus from '@/app/ui/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
+import { deleteInvoice } from '@/app/lib/actions';
 
 export default async function InvoicesTable({
   query,
